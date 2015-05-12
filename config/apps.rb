117a79df83ab9fd3db985c41911ec5cc6fc2c586
MoJie <onecoinim@foxmail.com>
2015-05-13 06:54:33 +0800

    添加 Emberjs app

--- config/apps.rb
+++ config/apps.rb
@@ -8,4 +8,7 @@ end
 Padrino.mount('ApiOnecoinIm::Api', :app_file => Padrino.root('api/app.rb')).to('/api')
 
 Padrino.mount("ApiOnecoinIm::Admin", :app_file => Padrino.root('admin/app.rb')).to("/admin")
-Padrino.mount('ApiOnecoinIm::OnecoinIm').to('/')
+
+Padrino.mount('ApiOnecoinIm::OnecoinIm', :app_file => Padrino.root('app/app.rb')).to('/app')
+
+Padrino.mount('ApiOnecoinIm::Emberjs', :app_file => Padrino.root('emberjs/app.rb')).to('/')
